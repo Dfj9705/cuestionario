24a2b6ba87732cdc294e47403c37b879201dacf1
javier58 <javier>
2022-12-19 08:35:46 -0600

    cambio select

--- assets/js/subtemas.js
+++ assets/js/subtemas.js
@@ -7,3 +7,32 @@ const iniciarModulo = (e) => {
     buttonModificar.parentElement.style.display = 'none'
     buttonModificar.disabled = true
 }
+
+
+// FUNCION PARA HACER SELECT
+const selectTemas = async () => {
+    const select = document.querySelector('#tema');
+    const url = "../API/temas/buscar.php";
+    const config = {
+        method : 'GET'
+    }
+
+    try {
+        const response = await fetch(url,config);
+        const data = await response.json();
+        const option = document.createElement('option');
+        option.innerText = "SELECCIONE...";
+        option.value = "";
+        select.appendChild(option);
+
+        data.forEach(d => {
+            const option = document.createElement('option');
+            option.innerText = d.descripcion;
+            option.value = d.jerarquia;
+            select.appendChild(option);
+        })
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
